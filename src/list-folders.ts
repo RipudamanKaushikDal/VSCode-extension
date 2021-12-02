df847e4ff508d2132c4cb8f378e37603b76f9ccc
Ripudaman Kaushik <rkaushik2021@rkaushik2021.local>
2021-12-02 14:27:26 -0400

    Semicolon formatting the list folders file

--- src/list-folders.ts
+++ src/list-folders.ts
@@ -4,34 +4,34 @@ const listFolders = async() => {
 
     try {
         
-        const workspace = vscode.workspace
-        const rootFolder = workspace.workspaceFolders
+        const workspace = vscode.workspace;
+        const rootFolder = workspace.workspaceFolders;
         
-        if (!rootFolder) return;
+        if (!rootFolder) {return;};
         
-        const pathConfig = workspace.getConfiguration('arrangeimports')
+        const pathConfig = workspace.getConfiguration('arrangeimports');
 
-        const folderDirectory:string =  pathConfig.get('folderDirectory') || '/src'
+        const folderDirectory:string =  pathConfig.get('folderDirectory') || '/src';
         
         const folderList:string[] = [];
 
-        const path:vscode.Uri = rootFolder[0]?.uri
-        const uri:vscode.Uri =  vscode.Uri.joinPath(path,folderDirectory)
-        const fileArray:[string,vscode.FileType][] = await vscode.workspace.fs.readDirectory(uri)
+        const path:vscode.Uri = rootFolder[0]?.uri;
+        const uri:vscode.Uri =  vscode.Uri.joinPath(path,folderDirectory);
+        const fileArray:[string,vscode.FileType][] = await vscode.workspace.fs.readDirectory(uri);
     
         fileArray.forEach((file) => {
             if (file[1] === 2){
-                const folderName:string = '// '+ file[0].toUpperCase()
-                folderList.push(folderName)
+                const folderName:string = '// '+ file[0].toUpperCase();
+                folderList.push(folderName);
             }
-        })
+        });
             
-        return folderList
+        return folderList;
 
     } catch (err) {
-        vscode.window.showErrorMessage("Not able to find folders, check 'Folder Directory' in extension settings")
+        vscode.window.showErrorMessage("Not able to find folders, check 'Folder Directory' in extension settings");
     }
 
-}
+};
 
-export default listFolders
+export default listFolders;
